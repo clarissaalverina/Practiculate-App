87e872617790b4436da7b2281aed622cd68aed1b
Clarissa Alverina <clarissaalverina97@gmail.com>
2024-05-23 14:25:44 +0700

    Initial Commit

---
+++ Practiculate Fix/Practiculate_FixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practiculate_FixApp.swift
+//  Practiculate Fix
+//
+//  Created by Clarissa Alverina on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Practiculate_FixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
